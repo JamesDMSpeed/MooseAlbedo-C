09fd337f99e2bf8097c9e6ae2e4381b1b18fa61e
allyworks <work@allyworks.io>
2019-12-30 16:34:51 +0100

    Update fsv.R

--- 1_Albedo_Exclosures/1_Data/FSV/fsv.R
+++ 1_Albedo_Exclosures/1_Data/FSV/fsv.R
@@ -8,6 +8,6 @@ library(lidR)
 
 
 #Get 'cleaned' site data from adjacent 'Sites' folder
-site_data <- read.csv('1_Albedo_Exclosures/1_Data/Sites/cleaned_data/cleaned_site_data.csv', header = TRUE)
+site_data <- read.csv('1_Albedo_Exclosures/1_Data/Sites/cleaned_data/cleaned_data.csv', header = TRUE)
 
 
